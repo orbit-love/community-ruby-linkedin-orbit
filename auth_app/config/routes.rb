6c86142b5d9059263e33d9934f050458d76a8df9
Ben Greenberg <ben@orbit.love>
2021-05-07 08:06:17 +0300

    add auth app

---
+++ auth_app/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: 'pages#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
